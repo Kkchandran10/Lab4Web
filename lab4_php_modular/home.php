ae6e4ecb3fb9145b6c7605747d6e0bd1c86903e7
Kkchandran10 <149285089+Kkchandran10@users.noreply.github.com>
2024-05-05 17:20:25 +0700

    Add files via upload

---
+++ lab4_php_modular/home.php
@@ -0,0 +1,6 @@
+<?php require('header.php'); ?>
+<div class="content">
+<h2>Ini Halaman Home Dari Test Website Koko Chandra Nuraga</h2>
+<p>Ini adalah bagian content dari halaman Website Koko.</p>
+</div>
+<?php require('footer.php'); ?>
---
+++ lab4_php_modular/kontak.php
@@ -0,0 +1,6 @@
+<?php require('header.php'); ?>
+<div class="content">
+<h2>Ini Halaman Kontak Dari Test Website Koko Chandra Nuraga</h2>
+<p>Ini adalah bagian content dari halaman Kontak Website Koko.</p>
+</div>
+<?php require('footer.php'); ?>
